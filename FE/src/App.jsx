4ba215c0e4bb3c30c91b64bc9301e9e4d8b6230b
quanglam04 <trinhquanglam2k4@gmail.com>
2025-03-23 10:13:52 +0700

    complete login and register page

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,80 +1,21 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Post from "./component/Post";
+import Header from "./layout/Header";
+import Layout from "./layout/Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">
-          MyBlog
-        </a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRemKmk_VwTJ3FI_DD4a7IC30-WF1NqrLm11A&s"
-            alt=""
-          />
-        </div>
-        <div className="text">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Trinh Quang Lam</a>
-            <time datetime="">2023-01-06 16:45</time>
-          </p>
-          <p className="summary">
-            Today ai its special launch event, home backup power giant EcoFlow
-            launched a flurry of new products. including a "Whole-home Backup
-            Power Solution"
-          </p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRemKmk_VwTJ3FI_DD4a7IC30-WF1NqrLm11A&s"
-            alt=""
-          />
-        </div>
-        <div className="text">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Trinh Quang Lam</a>
-            <time datetime="">2023-01-06 16:45</time>
-          </p>
-          <p className="summary">
-            Today ai its special launch event, home backup power giant EcoFlow
-            launched a flurry of new products. including a "Whole-home Backup
-            Power Solution"
-          </p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRemKmk_VwTJ3FI_DD4a7IC30-WF1NqrLm11A&s"
-            alt=""
-          />
-        </div>
-        <div className="text">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Trinh Quang Lam</a>
-            <time datetime="">2023-01-06 16:45</time>
-          </p>
-          <p className="summary">
-            Today ai its special launch event, home backup power giant EcoFlow
-            launched a flurry of new products. including a "Whole-home Backup
-            Power Solution"
-          </p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={"/login"} element={<LoginPage />} />
+        <Route path={"/register"} element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
