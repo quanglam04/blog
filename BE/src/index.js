f3a33738a4776057dd04fdce5dd33f7de99ebecb
quanglam04 <trinhquanglam2k4@gmail.com>
2025-03-23 10:57:43 +0700

    fix: cors

--- BE/src/index.js
+++ BE/src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
+//middleware
+app.use(express.json());
 app.get("/test", (req, res) => {
   res.json("test ok3333333333");
 });
 
 app.post("/register", (req, res) => {
-  res.json("test ok3");
+  const { username, password } = req.body;
+  res.json({ requestData: { username, password } });
 });
 app.listen(4000);
