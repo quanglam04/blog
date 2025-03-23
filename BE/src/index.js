54a1bff5814eb76a316c4d9e56e12e58e186383f
quanglam04 <trinhquanglam2k4@gmail.com>
2025-03-23 11:27:02 +0700

    create user

--- BE/src/index.js
+++ BE/src/index.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
 const app = express();
 
 app.use(cors());
 //middleware
 app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://trinhquanglam2k4:ryeLhByzda5nwk4W@cluster0.4qtgf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
 app.get("/test", (req, res) => {
   res.json("test ok3333333333");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  res.json({ requestData: { username, password } });
+  try {
+    const userDoc = await User.create({
+      username,
+      password,
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e.message);
+  }
 });
 app.listen(4000);
